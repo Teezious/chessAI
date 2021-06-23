85ed6fc338b9002de53cfe4b62a139686ec8b847
Matthias Sparr <mp.sparr@aon.at>
2021-06-23 15:39:42 +0200

    fixed quiecent search and sorting algorithm

--- src/ChessAI.h
+++ src/ChessAI.h
@@ -10,19 +10,22 @@
 #include <limits>
 
 #define SEARCH_DEPTH 5 // recommended 5 as 6 currently takes 30second + per move
-#define QUIESCENT_SEARCH_LIMIT 3
+#define QUIESCENT_SEARCH_LIMIT 4
 
 class ChessAI final : public AbstractChessPlayer
 {
   public:
     ChessAI(bool isWhite) : AbstractChessPlayer(isWhite){};
     std::string chooseMove(thc::ChessRules board, bool printResults = true);
-    std::string multiThreadedSearch(thc::ChessRules board, std::atomic<unsigned int> *nodesSearched, int *bestEval);
+    std::string multiThreadedSearch(thc::ChessRules board, std::atomic<unsigned int>* nodesSearched,
+                                    int* bestEval);
 
   protected:
   private:
-    static int minMax(thc::ChessRules& board, const int depth, bool maximize, int alpha, int beta, std::atomic<unsigned int> *nodesSearched);
-    static int quiescentSearch(thc::ChessRules& board, int alpha, int beta, std::atomic<unsigned int> *nodesSearched, int depth);
+    static int minMax(thc::ChessRules& board, const int depth, bool maximize, int alpha, int beta,
+                      std::atomic<unsigned int>* nodesSearched);
+    static int quiescentSearch(thc::ChessRules& board, int alpha, int beta,
+                               std::atomic<unsigned int>* nodesSearched, int depth);
 };
 
 #endif // CHESSAI_CHESSAI_H
